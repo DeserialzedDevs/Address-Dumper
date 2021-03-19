f9caa595a7b0c0ab4f4c4c927cf2ac292990f275
deadlocust <70506265+deaddlocust@users.noreply.github.com>
2021-03-19 13:51:45 +0800

    Update Program.cs

--- C#/Dumper/Program.cs
+++ C#/Dumper/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -14,41 +14,39 @@ namespace Dumper
         public static WebClient wc = new WebClient();
         public static Stopwatch watch = new Stopwatch();
         public static int addycount;
-        
+
         // AOBs
         public static string gettop = "55 8B EC 8B 4D 08 8B 41 ?? 2B 41 ?? C1 F8 04 5D"; /*this will never change*/
-	public static string index2adr = "55 8B EC 8B 55 ?? 81 FA F0 D8 FF FF 7E 0F ?? ?? ?? ?? E2 04 03 51 10 8B C2 5D C2 08 00 8B 45 08"; /*may break at some point*/
-	public static string retcheck = "55 8B EC 64 A1 00 00 00 00 6A ?? 68 E8 ?? ?? ?? ?? 64 89 25 00 00 00 00 83 EC ?? 53 56 57 6A ?? E9 ?? ?? ?? ??"; /*may break at some point*/
-	public static string deserialize = "55 8B EC 6A FF 68 70 ?? ?? ?? ?? A1 00 00 00 00 50 64 89 25 00 00 00 00 81 EC 58 01 00 00 56 57"; /*Again not 100% sure about this one's integrity*/
-	public static string getdatamodel = "55 8B EC 64 A1 00 00 00 00 6A FF 68 ?? ?? ?? ?? 50 64 89 25 00 00 00 00 83 EC ?? 80 3D 70 51 5E";
-        
+        public static string index2adr = "55 8B EC 8B 55 ?? 81 FA F0 D8 FF FF 7E 0F ?? ?? ?? ?? E2 04 03 51 10 8B C2 5D C2 08 00 8B 45 08"; /*may break at some point*/
+        public static string retcheck = "55 8B EC 64 A1 00 00 00 00 6A ?? 68 E8 ?? ?? ?? ?? 64 89 25 00 00 00 00 83 EC ?? 53 56 57 6A ?? E9 ?? ?? ?? ??"; /*may break at some point*/
+        public static string deserialize = "55 8B EC 6A FF 68 70 ?? ?? ?? ?? A1 00 00 00 00 50 64 89 25 00 00 00 00 81 EC 58 01 00 00 56 57"; /*Again not 100% sure about this one's integrity*/
+        public static string getdatamodel = "55 8B EC 64 A1 00 00 00 00 6A FF 68 ?? ?? ?? ?? 50 64 89 25 00 00 00 00 83 EC ?? 80 3D 70 51 5E";
+
         static void Main(string[] args)
         {
-		Console.Title = "C# Address Dumper";
-                Console.WriteLine("Scanning RBX " + wc.DownloadString("http://setup.roblox.com/version"));
-	        Console.WriteLine();
-                watch.Start();         
-            
-                // Scan AOBs
-                int gettop_addr = scanner.scan(gettop)[0];
-                int index2adr_addr = scanner.scan(index2adr)[0];
-                int retcheck_addr = scanner.scan(retcheck)[0];
-                int deserialize_addr = scanner.scan(deserialize)[0];
-            
-                // Log addresses
-                LogFunc("deserializer", util.raslr(deserialize_addr));
-                LogFunc("lua_gettop", util.raslr(gettop_addr));
-                LogFunc("index2adr", util.raslr(index2adr_addr));
-            
-                // More scanning
-                var retcheck_xrefs = scanner.scan_xrefs(retcheck_addr);
-                var index2adr_xrefs = scanner.scan_xrefs(index2adr_addr);
+            Console.Title = "C# Address Dumper";
+            Console.WriteLine("Scanning RBX " + wc.DownloadString("http://setup.roblox.com/version"));
+            Console.WriteLine();
+            watch.Start();
 
-                watch.Stop();
-                Console.WriteLine();
-                Console.WriteLine("Scanned " + addycount + " addresses" + " in " + watch.ElapsedMilliseconds + "ms");
-	        Console.WriteLine("Press Enter To Exit...");
-	        Console.ReadLine();
+            // Scan AOBs
+            int gettop_addr = scanner.scan(gettop)[0];
+            int index2adr_addr = scanner.scan(index2adr)[0];
+            int retcheck_addr = scanner.scan(retcheck)[0];
+            int deserialize_addr = scanner.scan(deserialize)[0];
+
+            // Log addresses
+            LogFunc("deserializer", util.raslr(deserialize_addr));
+            LogFunc("lua_gettop", util.raslr(gettop_addr));
+            LogFunc("index2adr", util.raslr(index2adr_addr));
+
+            // More scanning
+            var retcheck_xrefs = scanner.scan_xrefs(retcheck_addr);
+            var index2adr_xrefs = scanner.scan_xrefs(index2adr_addr);
+
+            watch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Scanned " + addycount + " addresses" + " in " + watch.ElapsedMilliseconds + "ms");
         }
 
         static void LogFunc(string fname, int addy)
